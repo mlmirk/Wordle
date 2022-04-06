1ea7e8008eba975fb006e402af0be62ed021b6f4
mlmirk <mike.l.mirkovic@gmail.com>
2022-04-06 09:22:50 -0700

    Created methods to track guesses and used words from the dictionary. We have updated run cycle of the app.

--- src/com/games/wordle/model/Player.java
+++ src/com/games/wordle/model/Player.java
@@ -1,10 +1,15 @@
 package com.games.wordle.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
     private final String name;
     private int wins;
     private int gamesPlayed;
+    private List<String> guesses = new ArrayList<>();
+    private List<Integer> randomIndex = new ArrayList<>();
 
     public Player(String name) {
         this.name = name;
@@ -38,6 +43,27 @@ public class Player {
     public void updateGamesPlayed() {
         this.gamesPlayed++;
     }
+
+    public void insertGuesses(String guess){
+        guesses.add(guess);
+    }
+    public boolean alreadyGuessed(String guess){
+        if(guesses.contains(guess)){
+            System.out.println("Word already guessed");
+        }
+        return !guesses.contains(guess);
+    }
+
+    public void insertIndexes(int index){
+        randomIndex.add(index);
+    }
+    public boolean indexUsed(int index){
+       return randomIndex.contains(index);
+
+
+
+    }
+
 
     @Override
     public String toString() {
